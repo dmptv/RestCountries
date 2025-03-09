1413afc2eeafec56f50a521d24ffa1e0721bc20f
Kanat Absadykov <absadykov.kanat@ext.careem.com>
2025-03-09 11:49:41 +0700

    use withTaskGroup for concurent removal

--- RestCountries/Preview Content/ImageURLCache.swift
+++ RestCountries/Preview Content/ImageURLCache.swift
@@ -8,8 +8,7 @@
 import UIKit
 import SwiftUI
 
-@MainActor
-final class ImageURLCache {
+actor ImageURLCache {
     static let shared = ImageURLCache()
 
     private var cache: [String: URL] = [:]
@@ -23,40 +22,39 @@ final class ImageURLCache {
     }
 
     func getURLString(forImageName name: String) async -> String? {
-        let result = "image not found"
         if let url = cache[name] {
             return url.absoluteString
         }
 
         guard let image = UIImage(named: name), let data = image.pngData() else {
             print("Image not found or could not get data from image")
-            return result
+            return nil
         }
 
         let tempFileURL = FileManager.default
             .temporaryDirectory
             .appendingPathComponent("\(name).png")
-
-        return await Task.detached {
-            let tempFileURL = FileManager.default
-                .temporaryDirectory
-                .appendingPathComponent("\(name).png")
-            do {
-                try data.write(to: tempFileURL)
-                await MainActor.run {
-                    self.cache[name] = tempFileURL
-                }
-                return tempFileURL.absoluteString
-            } catch {
-                print("Error writing to temporary file: \(error)")
-                return "image not found"
-            }
-        }.value
+        
+        do {
+            try data.write(to: tempFileURL)
+            cache[name] = tempFileURL
+            return tempFileURL.absoluteString
+        } catch {
+            return nil
+        }
     }
 
-    @objc func cleanup() async {
-        for url in cache.values {
-            try? FileManager.default.removeItem(at: url)
+    @objc
+    func cleanup() async {
+        await withTaskGroup(of: Void.self) { group in
+            for url in cache.values {
+                do {
+                    try FileManager.default.removeItem(at: url)
+                } catch {
+                    print("Error removing file: \(error)")
+                }
+            }
+            await group.waitForAll()
         }
         cache = [:]
     }
